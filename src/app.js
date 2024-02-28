8d90d2804c24f9ebc4c8934456e359b789795c3d
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-02-29 00:33:42 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
-function handleNextPage(e) {
-  e.preventDefault();
+function handleNextPage(event) {
+  event.preventDefault();
   const clientName = document.getElementById("name");
-  const emailAddress = document.getElementById("email");
+  const emailInput = document.getElementById("email");
+  const emailAddress = emailInput.value.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const clientNumber = document.getElementById("phone-number");
-  if (clientName && emailAddress && clientNumber) {
+  const errorMessage = document.querySelector(".error-message");
+  if (
+    clientName.length > 0 &&
+    clientNumber.length > 0 &&
+    emailAddress.length > 0 &&
+    emailPattern.test(emailAddress)
+  ) {
     const fisrtStepContainer = document.querySelector(".first-step-container");
     fisrtStepContainer.style.display = "none";
+    errorMessage.style.display = "none";
   } else {
     //error if no email and name
-    const errorMessage = document.querySelector(".error-message");
+
     errorMessage.style.display = "inline-block";
   }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const nextStep = document.querySelector(".btn-next-step");
-  nextStep.addEventListener("cliCK", handleNextPage);
+  nextStep.addEventListener("submit", handleNextPage);
 });
