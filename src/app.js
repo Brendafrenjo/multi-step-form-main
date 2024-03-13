87fdc67ebf16c4ee43cd6df7f11c387f6edc6710
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-03-13 23:31:52 +0300

    ft: Rendered select-plan-container responsive ft: Added active class to steps

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ function handleNextPage(event) {
   event.preventDefault();
   const clientName = document.getElementById("name").value;
   const emailInput = document.getElementById("email");
-  const selectPlan = document.querySelector("select-plan");
+  const selectPlan = document.querySelector(".select-plan");
   const fisrtStepContainer = document.querySelector(".first-step-container");
   const emailAddress = emailInput.value.trim();
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const clientNumber = document.getElementById("phone-number");
   const errorMessage = document.querySelector(".error-message");
   if (
+    //in case of successful processing
     clientName.length > 0 &&
     clientNumber.length > 0 &&
     emailAddress.length > 0 &&
@@ -19,15 +20,16 @@ function handleNextPage(event) {
     selectPlan.style.display = "block";
   } else {
     //error if no email and name
-
-    errorMessage.style.display = "inline-block";
+    errorMessage.style.display = "block";
   }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const nextStep = document.querySelector(".btn-next-step");
-  nextStep.addEventListener("submit", handleNextPage);
+  nextStep.addEventListener("click", handleNextPage);
 });
 
 const firstIcon = document.querySelector(".fa-1");
-firstIcon.classList.add(".active");
+firstIcon.classList.add("active-style");
+
+//so I have to swithc from one tab to the next and like that like that. I have no idea how I woll do it but lest's see.
