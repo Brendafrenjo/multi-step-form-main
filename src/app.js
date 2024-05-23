d134f2519861b635c1292f120ff303d0e6fcc6c0
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-05-23 13:05:18 +0300

    fix: Finihsing Up Page

--- src/app.js
+++ src/app.js
@@ -113,6 +113,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const secondGoBack = document.querySelector(".second-go-back");
   const thirdGoBack = document.querySelector(".confirm-go-back");
   const changeAccess = document.querySelector(".change-access");
+  const selectedPaymentSection = document.getElementById("");
+  const selectedAddOnSection = document.getElementById("");
+  const totalPerMonth = document.querySelector("");
+  const monthPriceElement = document.querySelector("");
+  const yearPriceElement = document.querySelector("");
+  const confirmButton = document.querySelector("");
 
   submitForm.addEventListener("submit", handleNextPage);
   firstGoBack.addEventListener("click", handlePrevious);
@@ -222,30 +228,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  function updateFinishingUp(planTitle, monthlyPrice, yearlyPrice) {
-    const finishingMonth = document.querySelector(".finishing-month");
-    const finishingYear = document.querySelector(".finishing-year");
-    const finishingSelectedPlan = document.querySelector(
-      ".finishing-selected-plan"
-    );
+  //retrieve selected payment plan from local storage
 
-    finishingSelectedPlan.textContent = `${planTitle} (Monthly)`;
-    finishingMonth.textContent = monthlyPrice;
-    finishingYear.textContent = yearlyPrice;
+  
 
-    const totalMonthlyPrice = parseInt(monthlyPrice.match(/\d+/)[0]) + 1;
-    const totalYearlyPrice = parseInt(yearlyPrice.match(/\d+/)[0]) + 10;
-    document.querySelector(".total-price-value").textContent =
-      totalMonthlyPrice;
-
-    const priceTotalMonthly = document.querySelector(
-      ".price-total.finishing-month"
-    );
-    const priceTotalYearly = document.querySelector(
-      ".price-total.finishing-year"
-    );
-
-    priceTotalMonthly.textContent = `+$${totalMonthlyPrice}/mo`;
-    priceTotalYearly.textContent = `+$${totalYearlyPrice}/yr`;
-  }
-});
+  });
